02500a3b1c82a0a48ec9adb4ecdb3bc7b671ce8b
Capucine Leclerc <capucine_49@hotmail.com>
2019-01-30 09:44:21 +0100

    first commit

---
+++ src/assets/data/cards-menu-data.model.ts
@@ -0,0 +1,6 @@
+export interface CardMenu {
+    label: string;
+    description: string;
+    image: string;
+    url: string;
+}
---
+++ src/assets/data/cards-menu-data.ts
@@ -0,0 +1,57 @@
+import { CardMenu } from './cards-menu-data.model';
+
+export const cardsMenuData: CardMenu[] = [
+  {
+    label: 'desserts',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/assets/images/desserts-home.jpg',
+    url: '#'
+},
+{
+    label: 'smoothies',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/assets/images/smoothies.jpg',
+    url: '#'
+},
+{
+    label: 'breakfast',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/assets/images/breakfast.jpg',
+    url: '#'
+},
+{
+    label: 'main dishes',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/assets/images/main-dishes.jpg',
+    url: '#'
+},
+{
+    label: 'soups',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/assets/images/soups.jpg',
+    url: '#'
+},
+{
+    label: 'hamburgers',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/assets/images/hamburgers.jpg',
+    url: '#'
+},
+{
+    label: 'desserts',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/assets/images/desserts-menu.jpg',
+    url: '#'
+},
+{
+    label: 'barbecue',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/assets/images/barbecue.jpg',
+    url: '#'
+},
+{
+    label: 'salads',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    image: '/assets/images/salads.jpg',
+    url: '#'
+}];
