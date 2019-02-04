e107b2bc1820bc9fbf5002f1b8b8618529103cd0
Capucine Leclerc <capucine_49@hotmail.com>
2019-02-04 15:59:35 +0100

    images + debut cartes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {SlideshowModule} from 'ng-simple-slideshow';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './header/menu/menu.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
