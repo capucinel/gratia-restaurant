a01a91d2589d3286e4863fc04292433c9b6ac6c0
Capucine Leclerc <capucine_49@hotmail.com>
2019-02-06 18:06:11 +0100

    one card with hover

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+hoverOn: boolean;
+hoverOff: boolean;
 
-  constructor() { }
+hover() {
+  this.hoverOn = !this.hoverOn;
 
+  console.log(this.hoverOn);
+}
+
+hoverO() {
+  this.hoverOn = !this.hoverOn;
+console.log("off = " + this.hoverOn);
+
+}
+  getUrl() {
+    return 'url(assets/images/desserts-home.jpg)';
+  }
   ngOnInit() {
+  this.hoverOn = false;
+  this.hoverOff = true;
   }
 
 }
