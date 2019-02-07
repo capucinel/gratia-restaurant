f5a254b98f8b0109c5e73916a59fee66c68c06e9
Capucine Leclerc <capucine_49@hotmail.com>
2019-02-07 15:47:33 +0100

    end card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,69 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { cardsMenuData } from '../../assets/data/cards-menu-data';
 
 @Component({
   selector: 'app-card',
   animations: [
     trigger('cardHover', [
       state('hoverOff', style({
-        height: '325px',
+        'height': '325px',
+        'transform': 'scale(1)',
+        'boxShadow': 'none',
       })),
       state('hoverOn', style({
-        height: '400px',
+        'height': '330px',
+        'transform': 'scale(1.1)',
+        'boxShadow': '10px -10px 25px 0 rgba(0, 0, 0, 0.1)'
       })),
       transition('hoverOff => hoverOn', [
         animate('0.4s')
       ]),
       transition('hoverOn => hoverOff', [
-        animate('0.2s')
-      ]),
-    ]),
-
-    trigger('layer', [
-      state('hoverOff', style({
-        'backgroundColor': 'rgba(0, 0, 0, 0.6)'
-      })),
-      state('hoverOn', style({
-        'backgroundColor': 'transparent',
-      })),
-      transition('hoverOff => hoverOn', [
         animate('0.4s')
       ]),
-      transition('hoverOn => hoverOff', [
-        animate('0.2s')
-      ]),
     ]),
-
     trigger('title', [
       state('hoverOff', style({
-        'top': '15%',
+        'top': '35%',
         'padding-bottom': '25px'
       })),
       state('hoverOn', style({
-        'top': '45%',
+        'top': '55%',
         'padding-bottom': '15px'
       }))
     ]),
 
     trigger('description', [
       state('hoverOff', style({
-        'top': '30%',
+        'top': '55%',
       })),
       state('hoverOn', style({
-        'top': '60%',
+        'top': '75%',
       }))
     ]),
 
     trigger('border', [
       state('hoverOff', style({
         'color': '#d89b64',
-        'top': '25%',
+        'top': '47%',
 
       })),
       state('hoverOn', style({
         'color': '#151b29',
-        'top': '55%',
-
+        'top': '67%',
       }))
     ]),
   ],
@@ -72,6 +60,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 })
 export class CardComponent implements OnInit {
   hoverOff: boolean;
+  dataMenus = cardsMenuData;
 
   hover() {
     this.hoverOff = !this.hoverOff;
